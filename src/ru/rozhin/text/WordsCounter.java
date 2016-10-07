921486ae511884b875149bee7982f5ce7b965d5e
ARozhin <alexey.rozhin90@gmail.com>
2016-10-07 14:36:40 +0300

    count same words

--- src/ru/rozhin/text/WordsCounter.java
+++ src/ru/rozhin/text/WordsCounter.java
@@ -11,13 +11,28 @@ public class WordsCounter {
 
         int n = input.length();
         for (int i = 0; i < n; i++) {
-            letter = input.substring(i, i+1);
-            if (!letter.equals(",")) {
+            letter = input.substring(i, i + 1);
+            if (!isDelimeter(letter)) {
                 word = word.concat(letter);
             } else {
-                result.put(word, 1);
+                if (result.containsKey(word))
+                    result.put(word, result.get(word) + 1);
+                else
+                    result.put(word, 1);
+                word = "";
             }
         }
         return result;
+    }
+
+    private final static String[] delimiters = {",", " ", ".", ";", "-", "?", "!"};
+
+    private static boolean isDelimeter(String symbol) {
+        int n = delimiters.length;
+        for (int i = 0; i < n; i++) {
+            if (symbol.equals(delimiters[i]))
+                return true;
+        }
+        return false;
     }
 }
